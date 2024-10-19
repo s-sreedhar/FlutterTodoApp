7cffd01a564db2217883bfe6644d9ac3b91b2f18
S Sreedhar Goud <s.sreedhargoud@gmail.com>
2024-10-19 22:53:34 +0530

    first commit

---
+++ lib/utils/todo_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({super.key, required this.taskName, required this.taskCompleted, required this.onChanged, required this.deleteFuncion});
+  
+  final String taskName; 
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
+   final Function(BuildContext)? deleteFuncion;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: const EdgeInsets.only(top:20,
+        left:20,
+        right:20,
+        bottom:0),
+         child : Slidable(
+          endActionPane: ActionPane(motion: const StretchMotion(),
+           children: [SlidableAction(onPressed: deleteFuncion,
+           icon: Icons.delete,
+           borderRadius: BorderRadius.circular(15),
+           backgroundColor: Colors.red,
+           ),]),
+           child: Container(
+            padding:const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(children: [
+              Checkbox(
+                value: taskCompleted, onChanged: onChanged,
+                checkColor: Colors.black,
+                activeColor: Colors.white,
+                side: const BorderSide(
+                  color: Colors.white,
+                )),
+                Text(
+              style:  TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                decoration: taskCompleted ? TextDecoration.lineThrough: TextDecoration.none,
+                decorationColor: Colors.white,
+                decorationThickness: 2,
+              ),
+              taskName),
+              
+            ],)
+                   
+                   ),
+         )
+        );
+  }
+}
